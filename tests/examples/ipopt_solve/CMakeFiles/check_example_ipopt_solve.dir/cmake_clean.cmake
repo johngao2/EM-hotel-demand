c680eba2be96a3499153080f6340cdb03718a803
johngao2 <john.gao@queensu.ca>
2019-02-13 18:21:20 -0500

    IT COMPILED

---
+++ tests/examples/ipopt_solve/CMakeFiles/check_example_ipopt_solve.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/check_example_ipopt_solve"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/check_example_ipopt_solve.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tests/examples/ipopt_solve/CMakeFiles/example_ipopt_solve.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/johngao/work/thesis/cppad-20180000.0/example/ipopt_solve/get_started.cpp" "/Users/johngao/work/thesis/cppad-20180000.0/build/example/ipopt_solve/CMakeFiles/example_ipopt_solve.dir/get_started.cpp.o"
+  "/Users/johngao/work/thesis/cppad-20180000.0/example/ipopt_solve/ipopt_solve.cpp" "/Users/johngao/work/thesis/cppad-20180000.0/build/example/ipopt_solve/CMakeFiles/example_ipopt_solve.dir/ipopt_solve.cpp.o"
+  "/Users/johngao/work/thesis/cppad-20180000.0/example/ipopt_solve/ode_inverse.cpp" "/Users/johngao/work/thesis/cppad-20180000.0/build/example/ipopt_solve/CMakeFiles/example_ipopt_solve.dir/ode_inverse.cpp.o"
+  "/Users/johngao/work/thesis/cppad-20180000.0/example/ipopt_solve/retape.cpp" "/Users/johngao/work/thesis/cppad-20180000.0/build/example/ipopt_solve/CMakeFiles/example_ipopt_solve.dir/retape.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "prefix/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
